4d7bfa5cea8712e611d03803e75d821385eacc70
Fiona Healy <fionahealy17@gmail.com>
2020-09-01 18:18:27 +0200

    input split works but now need to come up with way to correctly parse/interpret user input

--- p_and_s_17.py
+++ p_and_s_17.py
@@ -17,19 +17,19 @@ red_balls = balls["red"]
 blue_balls = balls["blue"]
 white_balls = balls["white"]
 
+#
 event_split = events.split()
+print(event_split)
 
 for event in event_split:
 	event_value = balls[event]
-	print(event_value)
 
-# event_split = event.split()
-# print(event_split)
-# nots = "not" in event_split
-# ors = "or" in event_split
+
 
 
 p_red = red_balls/total_balls
+
+
 
 
 
