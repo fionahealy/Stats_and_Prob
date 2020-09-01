fb259fe65b8af6d461d79882bd216295446cff04
Fiona Healy <fionahealy17@gmail.com>
2020-09-01 17:55:07 +0200

    changed numbers to ints, added user input, began event parsing process

--- p_and_s_17.py
+++ p_and_s_17.py
@@ -1,21 +1,25 @@
 import math
 import numpy
 
+event = input("Enter event:")
+
 balls = {
-	"blue":5,
-	"red":6,
-	"white":4
+	"blue":5.0,
+	"red":6.0,
+	"white":4.0
 }
 
 balls_list = list(balls.values())
 
 total_balls = sum(balls_list)
 
-red_balls = balls['red']
-blue_balls = balls['blue']
-white_balls = balls['white']
+red_balls = balls["red"]
+blue_balls = balls["blue"]
+white_balls = balls["white"]
 
-p_red = red_balls/total_balls
+event_value = balls[event]
+print(event_value)
+
 
 
 
