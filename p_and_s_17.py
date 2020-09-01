79043a8fa6fdb7298e6a48147f3f9bb096e7c671
Fiona Healy <fionahealy17@gmail.com>
2020-09-01 17:13:01 +0200

    added ability to sum total amount of balls

--- p_and_s_17.py
+++ p_and_s_17.py
@@ -1,11 +1,17 @@
 import math
 import numpy
 
+
+
 balls = {
 	"blue":5,
 	"red":6,
 	"white":4
 }
 
-print(list(balls.values()))
+balls_list = list(balls.values())
+
+total_balls = sum(balls_list)
+
+print(total_balls)
 
