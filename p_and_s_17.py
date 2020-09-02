65cc1e0e3289d6ec3d4bb7557ec0d944d6bc4d85
Fiona Healy <fionahealy17@gmail.com>
2020-09-02 06:17:39 +0200

    some tidying

--- p_and_s_17.py
+++ p_and_s_17.py
@@ -13,21 +13,12 @@ balls_list = list(balls.values())
 
 total_balls = sum(balls_list)
 
-red_balls = balls["red"]
-blue_balls = balls["blue"]
-white_balls = balls["white"]
-
-#
 event_split = events.split()
 print(event_split)
 
 for event in event_split:
 	event_value = balls[event]
 
-
-
-
-p_red = red_balls/total_balls
 
 
 
