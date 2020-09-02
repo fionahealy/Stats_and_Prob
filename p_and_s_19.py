824ceefc16a71e5add51c0ae553f51e26c006a8d
Fiona Healy <fionahealy17@gmail.com>
2020-09-02 18:33:00 +0200

    added a lot of generalization

--- p_and_s_19.py
+++ p_and_s_19.py
@@ -1,17 +1,35 @@
 import random
 from matplotlib import pyplot
+import numpy
+from collections import Counter
 
 sums = []
+die_rolls = 10
+plot_range = die_rolls*6+1
 
-for i in range(0,100):
+for i in range(0,100000):
 
-	die_roll_1 = random.randint(1,6)
-	die_roll_2 = random.randint(1,6)
+	rolls = []
 
-	sum_dice = die_roll_2+die_roll_1
+	for roll in range(1,die_rolls+1):
+
+		die_roll = random.randint(1,6)
+		rolls.append(die_roll)
+
+	sum_dice = numpy.sum(rolls)
 
 	sums.append(sum_dice)
 
-pyplot.plot(sums)
-pyplot.show()
+# pyplot.plot(sums)
+# pyplot.show()
 
+counts = []
+for i in range(1,plot_range):
+	count = sums.count(i)
+	counts.append(count)
+
+print(counts)
+
+pyplot.plot(numpy.arange(1,plot_range),counts)
+pyplot.grid()
+pyplot.show()
