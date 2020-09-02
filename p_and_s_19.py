e0a6e0aad76456fc4ee8be5d9c4982e3557295ce
Fiona Healy <fionahealy17@gmail.com>
2020-09-03 00:05:27 +0200

    integrated gaussian fit to plot

--- p_and_s_19.py
+++ p_and_s_19.py
@@ -5,10 +5,10 @@ from scipy import optimize
 from astropy.modeling import models, fitting
 
 sums = []
-die_rolls = 2
+die_rolls = 100
 plot_range = die_rolls*6+1
 
-for i in range(0,1000):
+for i in range(0,10000):
 
 	rolls = []
 
@@ -31,8 +31,10 @@ for i in range(1,plot_range):
 
 pyplot.plot(numpy.arange(1,plot_range),counts)
 pyplot.grid()
-pyplot.show()
 
-g_init = models.Gaussian1D(amplitude=1., mean=0, stddev=1.)
+g_init = models.Gaussian1D(amplitude=100., mean=350, stddev=50.)
 fit_g = fitting.LevMarLSQFitter()
 g = fit_g(g_init, numpy.arange(1,plot_range), counts)
+
+pyplot.plot(numpy.arange(1,plot_range),g(numpy.arange(1,plot_range)))
+pyplot.show()
