07516265c93c15b0e488a492502cc62e16bafc88
Fiona Healy <fionahealy17@gmail.com>
2020-09-02 06:40:45 +0200

    added parenthesis finding function with test

--- p_and_s_17.py
+++ p_and_s_17.py
@@ -1,6 +1,10 @@
 import math
 import numpy
 
+def parenthesis_finder(str):
+	inner_str = str[str.find("(")+1:str.rfind(")")]
+	return inner_str
+
 events = input("Enter event:")
 
 balls = {
@@ -13,12 +17,21 @@ balls_list = list(balls.values())
 
 total_balls = sum(balls_list)
 
-event_split = events.split()
-print(event_split)
+inside_parenthesis = parenthesis_finder(events)
+print(inside_parenthesis)
 
-for event in event_split:
-	event_value = balls[event]
+# event_split = events.split()
+# print(event_split)
 
+# for event in event_split:
+# 	event_value = balls[event]
+
+
+
+
+#examples: red, red or white, red and not white, not (blue or white),
+#(not blue) or white, red and (not white or blue), red or not white,
+#(red and not white) or blue
 
 
 
