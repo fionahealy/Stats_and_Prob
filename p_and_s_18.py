2405c3c35997761c6e59fd0f4d968dbcabd405d7
Fiona Healy <fionahealy17@gmail.com>
2020-09-02 17:48:52 +0200

    finished while loop and added total prob at end - code now operational

--- p_and_s_18.py
+++ p_and_s_18.py
@@ -1,6 +1,19 @@
 
-die_roll = input("Roll the die?")
-if die_roll == "Y":
-	bet = input("Place your bet: ")
+import numpy
+
+die_roll = input("Roll the die? (Y/N) ")
 
 die_sample_space = 6
+
+probs = []
+
+while die_roll == "Y":
+	bet = input("Place your bet: (1 - 6, comma separated) ")
+	bet_list = bet.split(",")
+	probs.append(len(bet_list)/die_sample_space)
+	roll_again = input("Roll again? (Y/N) ")
+	die_roll = roll_again
+
+total_prob = numpy.prod(probs)
+print(total_prob)
+
