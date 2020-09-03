2091adf3336adce96208bd972dd2b7503d1618a6
Fiona Healy <fionahealy17@gmail.com>
2020-09-03 23:12:41 +0200

    tidied up and took out all gaussian fit stuff (to try again)

--- p_and_s_19.py
+++ p_and_s_19.py
@@ -1,11 +1,12 @@
 import random
 import numpy
 from matplotlib import pyplot
-from scipy.stats import norm
 
 sums = []
 die_rolls = 3
+
 plot_range = die_rolls*6+1
+x_range = numpy.arange(1,plot_range)
 
 for i in range(0,1000):
 
@@ -20,19 +21,12 @@ for i in range(0,1000):
 
 	sums.append(sum_dice)
 
-# pyplot.plot(sums)
-# pyplot.show()
 
 counts = []
 for i in range(1,plot_range):
 	count = sums.count(i)
 	counts.append(count)
 
-mean,std=norm.fit(counts)
-print(mean)
-y = norm.pdf(numpy.arange(1,plot_range), mean, std)
 
-pyplot.plot(numpy.arange(1,plot_range),counts)
-pyplot.grid()
-pyplot.plot(numpy.arange(1,plot_range),y)
+pyplot.plot(x_range,counts)
 pyplot.show()
