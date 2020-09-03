553695a857ba7389d619b15a448328d0f5b20385
Fiona Healy <fionahealy17@gmail.com>
2020-09-03 23:16:27 +0200

    add comments

--- p_and_s_19.py
+++ p_and_s_19.py
@@ -2,11 +2,15 @@ import random
 import numpy
 from matplotlib import pyplot
 
+#--------init-----------------------------------
+
 sums = []
 die_rolls = 3
 
 plot_range = die_rolls*6+1
 x_range = numpy.arange(1,plot_range)
+
+#--------die-rolls------------------------------
 
 for i in range(0,1000):
 
@@ -21,12 +25,14 @@ for i in range(0,1000):
 
 	sums.append(sum_dice)
 
+#--------analyse-data----------------------------
 
 counts = []
 for i in range(1,plot_range):
 	count = sums.count(i)
 	counts.append(count)
 
+#--------plot-data-------------------------------
 
 pyplot.plot(x_range,counts)
 pyplot.show()
